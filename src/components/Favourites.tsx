8ac2f4c67e63e3cd5e498d32ad773d6594f0e012
SkiuteDomantas <domantas.skiute@student.howest.be>
2026-07-17 20:50:49 +0300

    Fixed bug with rates not saving to the localStorage

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -1,11 +1,54 @@
-export default function Favourites({favourites}){
+import { useEffect, useState } from "react";
+
+export default function Favourites({ favourites }) {
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    async function fetchRates() {
+      const newRates = {};
+
+      for (const fav of favourites) {
+        try {
+          const response = await fetch(
+            `https://api.frankfurter.dev/v2/rates?base=${fav.base}&quotes=${fav.quote}`
+          );
+
+          if (!response.ok) {
+            throw new Error("Something went wrong");
+          }
+
+          const data = await response.json();
+
+          newRates[fav.id] = data[0].rate;
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+
+      setRates(newRates);
+    }
+
+    if (favourites.length > 0) {
+      fetchRates();
+    }
+  }, [favourites]);
+
   return (
-    <>
     <ul className="ml-70 mt-10 flex flex-col gap-8">
-      {favourites.map((fav)=>(
-        <li className="px-8 py-6 flex rounded-md items-center justify-between max-w-56 bg-neutral-900" key={fav.id}><span className="text-sm text-neutral-500">{fav.base}/{fav.quote}</span><span className="text-lg">{(fav.rate).toFixed(4)}</span></li>
+      {favourites.map((fav) => (
+        <li
+          key={fav.id}
+          className="px-8 py-6 flex rounded-md items-center justify-between max-w-56 bg-neutral-900"
+        >
+          <span className="text-sm text-neutral-500">
+            {fav.base}/{fav.quote}
+          </span>
+
+          <span className="text-lg">
+            {rates[fav.id] ?? "..."}
+          </span>
+        </li>
       ))}
     </ul>
-    </>
-  )
+  );
 }
